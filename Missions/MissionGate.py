725ff564de9e8b3cb136599269484d5c00f4ca54
FernandoLGuzman <31297194+FernandoLGuzman@users.noreply.github.com>
2017-12-18 19:29:11 -0400

    added missiongate

---
+++ Missions/MissionGate.py
@@ -0,0 +1,29 @@
+import math
+angulo = math.acos(w)
+w = adj/hyp
+
+def seeGate():
+    return boolean1
+
+def seeRedStrips():
+    return boolean2
+
+def rotate():
+    while seeGate() == False:
+        """"rotate 60 degrees"""
+
+"""def moveForward():
+    while seeGate() == True and seeRedStrips()== False:
+        move foward"""
+
+def redStrips():
+    while seeRedStrips == True and angulo == 90:
+
+        """stop moving forward, move towards red side of gate."""
+
+
+
+
+
+
+
