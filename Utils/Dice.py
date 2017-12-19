15b0f731909084110e4ce7861d686b413ddff8ac
Gabriel A. Valentín <31459082+gabrielvalentin@users.noreply.github.com>
2017-12-18 19:27:15 -0500

    Objects for the dice mission

---
+++ Utils/Dice.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Gabriel Valentin
+"""
+
+
+class Dice:
+
+    def __init__(self):
+
+        xcoordinate = 1
+        ycoordinate = 0
+        subdistance = 0
+        numberofdice = 0
+
+"""Setters"""
+    def setterX(self, recieved):
+        self.recievedThing = recieved
+
+    def setterY(self, recieved):
+    self.recievedThing = recieved
+
+    def setterDistance(self, recieved):
+    self.recievedThing = recieved
+
+    def setterNumber(self, 5):
+    self.recievedThing = 5
+
+
+
+
+"""Getters"""
+    def getterX(self):
+        return self.recievedThing
+
+    def getterY(self):
+        return self.recievedThing
+
+    def getterDistance(self):
+        return self.recievedThing
+
+    def getterNumber(self):
+        return self.recievedThing
+
