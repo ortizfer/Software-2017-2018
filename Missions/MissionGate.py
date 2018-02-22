38b0a035fdbb7e7c6af74b3d82b67aa23ccc680b
FernandoLGuzman <31297194+FernandoLGuzman@users.noreply.github.com>
2018-02-21 20:01:48 -0400

    few changes to  missiongate

--- Missions/MissionGate.py
+++ Missions/MissionGate.py
@@ -2,15 +2,18 @@ import math
 from Utils import Movement
 from Utils.Gate import Gate
 
-def seeGate(bool1):
-    if bool1==False:
-        Movement.rotate(70)
-        seeGate()
-"""        return False
-    else:
-        return True"""
+camCenterX = 480/2
+camCenterY = 640/2
 
-def allign(d,theta):
+bool1 = False #for identifying if gate is in frame
+
+def see_gate():
+    if bool1:
+        Movement.rotate(70)
+        see_gate()
+
+
+def allign():
     a=math.cos(theta)*d
     if theta < 0:
         Movement.left(a)
@@ -22,8 +25,3 @@ def allign(d,theta):
         Movement.forward(d)
 
 def main():
- """   boolean = seeGate()
-    while boolean == False:
-        boolean = seeGate()"""
-    seeGate()
-    allign()
