57eb788782390252da2fa083e98d5da7f7e516a9
FernandoLGuzman <31297194+FernandoLGuzman@users.noreply.github.com>
2018-02-28 19:54:07 -0400

    added binding box checker

--- Missions/MissionGate.py
+++ Missions/MissionGate.py
@@ -7,6 +7,12 @@ camCenterY = 640/2
 
 bool1 = False #for identifying if gate is in frame
 
+def check_binding_box(x,y):
+    if (camCenterX-50) < x < (camCenterX+50) and (camCenterY-50) < y < (camCenterY+50):
+        return True
+    return False
+
+
 def see_gate():
     if bool1:
         Movement.rotate(70)
