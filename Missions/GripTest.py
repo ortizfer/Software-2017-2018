a558446ee772238a51f2cf763e881c195805b7cd
FernandoLGuzman <31297194+FernandoLGuzman@users.noreply.github.com>
2018-03-03 12:12:00 -0400

    added grip test

---
+++ Missions/GripTest.py
@@ -0,0 +1,14 @@
+# import grip
+from Utils import Movement
+
+# grip.close
+Movement.forward(3)
+# grip.extend
+Movement.rotate(20)
+# grip.open
+Movement.forward(1)
+# grip.close
+Movement.rotate(90)
+# grip.retract
+# grip.open
+
