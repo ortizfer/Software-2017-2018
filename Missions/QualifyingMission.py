a3791e9fce699e5a248435ba798dc732de2c6522
yanis2014 <34663632+yanis2014@users.noreply.github.com>
2018-04-14 16:10:41 -0400

    updating files

--- Missions/QualifyingMission.py
+++ Missions/QualifyingMission.py
@@ -94,13 +94,13 @@ def runQualifying():
 
         if abs(xGate - xPixels/2) > boxRadius:
             if xGate - xPixels/2 > 0:
-                RosCom.headingMotors(1,1,90)
-                move("F",40,1)
-                RosCom.headingMotors(1,1,-90)
+                RosCom.headingMotors(1, 1, 90)
+                move("F", 40, 1)
+                RosCom.headingMotors(1, 1, -90)
             else:
-                RosCom.headingMotors(1,1,-90)
-                move("F",40,1)
-                RosCom.headingMotors(1,1,90)
+                RosCom.headingMotors(1, 1, -90)
+                move("F", 40, 1)
+                RosCom.headingMotors(1, 1, 90)
         else:
             move("F", 40, 6)
         seeGateVision()
@@ -148,6 +148,7 @@ def runQualifying():
             else:
                 RosCom.moveFoward(20)
         else:
+            global counter
             RosCom.headingMotors(1, 0, 0)
             RosCom.headingMotors(1, 1, turnDegree)
             counter = counter + turnDegree
@@ -171,13 +172,13 @@ def runQualifying():
 
         if abs(xGate - xPixels/2) > boxRadius:
             if xGate - xPixels/2 > 0:
-                RosCom.headingMotors(1,1,90)
-                move("F",40,1)
-                RosCom.headingMotors(1,1,-90)
+                RosCom.headingMotors(1, 1, 90)
+                move("F", 40, 1)
+                RosCom.headingMotors(1, 1, -90)
             else:
-                RosCom.headingMotors(1,1,-90)
-                move("F",40,1)
-                RosCom.headingMotors(1,1,90)
+                RosCom.headingMotors(1, 1, -90)
+                move("F", 40, 1)
+                RosCom.headingMotors(1, 1, 90)
         else:
             RosCom.moveFoward(40)
         seeGateVision()
