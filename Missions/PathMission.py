5a12a5e06a651b0274cd71b3f94ccb582a1b3574
yanis2014 <34663632+yanis2014@users.noreply.github.com>
2018-04-14 15:47:52 -0400

    updating files

--- Missions/PathMission.py
+++ Missions/PathMission.py
@@ -1,65 +1,10 @@
-from Utils import Movement
-from Utils import Path
 """
 
 
 @author: Carlos Morel and Tatiana Rodriguez
 """
-#Code
-# Definitions
-#Definitions from Vision
-def frontCamFoundPath():
-    global foundPath
-    # VISION
-    return #HERE WE WILL USE A VARIABLE THAT RETURN A TRUE OR FALSE
 
-def bottomCamFoundPath():
-    global foundPathBottom
-    #VISION
-    return
-
-def searchForPath(x): #Search for the path
-    global foundPath
-    if not x:
-        foundPath = frontCamFoundPath()
-        if not foundPath:
-            Movement.rotate(60)
-            foundPath = frontCamFoundPath()
-            if not foundPath:
-                Movement.rotate(-120)
-
-
-def searchForPathBottom(y): #Find path with the bottom camera
-    global foundPathBottom
-    count = 0
- '''   while not y and count < 6:
-        Movement.rotate(60)
-        bottomCamFoundPath(foundPathBottom)
-        count += 1
-    return foundPathBottom'''
-
-foundPath = False #Initial value that will make the submarine look for the path
-foundPathBottom = False
-
-Movement.depth(3.9624) #Submerge so we are able to see the path with the frontal camera
-
-if not foundPath: #if I dont see the path with the frontal camera look right
-    global foundPath
-    Movement.rotate(60) #Do not know what function use in this part
-    frontCamFoundPath(foundPath)
-    searchForPath(foundPath)
-
-
-Cameras.onDownCamera    #Command to turn on the down camera
-
-
-
-while (not foundPathBottom):
-    global foundPathBottom
-    Movement.foward() #By how many meters should the submarine move foward
-    searchForPathBottom(foundPathBottom)
-
-"""CMOREL NEW PATH BEGINNING
+CMOREL NEW PATH BEGINNING
 
 # Imports
 from Utils import Path
@@ -75,15 +20,13 @@ def seePathVision():
     global seePath
     seePath = Path.getseePath()
 
-
-
 # Mission Logic
 
 """Tells VISION in what mission we are"""
 RosCom.setVisionMission(2)
 
 """Initialize seePath variable"""
-seePathVision()  # Can I see the path from the bottom camera at our initial depth
+seePathVision()
 
 """What Charlie thinks the mission will continue
 WAITING ON THE STUPED HOE FROM TATS"""
@@ -91,5 +34,4 @@ WAITING ON THE STUPED HOE FROM TATS"""
 """After seeing the path with the bottom camera
 at the initial depth"""
 
-RosCom.setDepth(enable,12)  # Go down until we are 12 feet
-"""
+RosCom.setDepth(True, 12)  # Go down until we are 12 feet
