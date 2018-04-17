76561c770dbe3eb1e13e508bfcf0d942a1bc56a7
clopez26 <carlos.lopez26@upr.edu>
2018-04-16 21:05:21 -1200

    commented sensor functions - Carlos Lopez

--- Arduino_Sketchbook/ArduROS/ArduROS.ino
+++ Arduino_Sketchbook/ArduROS/ArduROS.ino
@@ -3,17 +3,19 @@
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Int32.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Int32MultiArray.h>
 #include <Motors.h>
 #include <Wire.h>
 #include <MS5837.h>
 
-int leftMotor = 0; 
-int rightMotor = 0;
+//int leftMotor = 0; 
+//int rightMotor = 0;
 std_msgs::Float32 depth;
-MS5837 pressureSensor;
+//MS5837 pressureSensor;
 
 // Callback function for left motors
-void setLeftMotors(const std_msgs::Int32&  motorIntensity)
+
+/*void setLeftMotors(const std_msgs::Int32&  motorIntensity)
 {
    leftMotor = motorIntensity.data;
    setHorizontalMotorSpeed(motorIntensity.data, rightMotor);
@@ -26,36 +28,47 @@ void setRightMotors(const std_msgs::Int32& motorIntensity)
     setHorizontalMotorSpeed(leftMotor, motorIntensity.data);
 }
 
-// Callback function for simultaneous usage of motors
+*/
+
+// Callback function for simultaneous usage of vertical motors
 void setVerticalMotors(const std_msgs::Int32& motorIntensity)
 {
     setVerticalMotorSpeed(motorIntensity.data);
 }
 
+// Callback function for simultaneous usage of horizontal motors
+void setHorizontalMotors(const std_msgs::Int32MultiArray& message)
+{
+    setHorizontalMotorSpeed(message.data[0], message.data[1]);
+}
+
 
 ros::NodeHandle nh;
 
-// Creates all the ROS subscriber objects 
+// Creates all the ROS subscriber objects
+ros::Subscriber<std_msgs::Int32MultiArray> subHorizontalMotors("horizontal_motors", setHorizontalMotors);
 ros::Subscriber<std_msgs::Int32> subVerticalMotors("vertical_motors", setVerticalMotors);
-ros::Subscriber<std_msgs::Int32> subLeftMotors("left_motor", setLeftMotors);
-ros::Subscriber<std_msgs::Int32> subRightMotors("right_motor", setRightMotors);
+//ros::Subscriber<std_msgs::Int32> subLeftMotors("left_motor", setLeftMotors);
+//ros::Subscriber<std_msgs::Int32> subRightMotors("right_motor", setRightMotors);
 
 // Create the pressure sensor publisher 
-ros::Publisher pubToPressureSensor("pressure_sensor", &depth);
+//ros::Publisher pubToPressureSensor("pressure_sensor", &depth);
 
 
 void setup() {
   // Initializes the node and prepares for publishing and subscribing.
   nh.initNode();
-  nh.advertise(pubToPressureSensor);
+ // nh.advertise(pubToPressureSensor);
   nh.subscribe(subVerticalMotors);
-  nh.subscribe(subLeftMotors);
-  nh.subscribe(subRightMotors);
+  nh.subscribe(subHorizontalMotors);
+  //nh.subscribe(subLeftMotors);
+ // nh.subscribe(subRightMotors);
 
   // Initializes all the motors
   initializeVerticalMotors();
   initializeHorizontalMotors("old");
 
+/*
   // Initializes wire (protocol to run)
   Wire.begin(); 
 
@@ -64,15 +77,17 @@ void setup() {
   {
       nh.logerror("Pressure sensor cannot initialize.");
   }
+
+*/
 }
 
 void loop() 
 {
-  pressureSensor.read();
+  /* pressureSensor.read();
 
   depth.data = pressureSensor.depth()/(3.3);
 
-  pubToPressureSensor.publish(&depth);
+  pubToPressureSensor.publish(&depth); */
   
   nh.spinOnce();
 }
