bc252db15cc18bf6e1ee3d3e58cf3dccdc922ab5
Gabriel A. Valentín <31459082+gabrielvalentin@users.noreply.github.com>
2018-04-30 22:19:25 -0500

    BigDice function implemented and errors fixed

--- Missions/DiceMission.py
+++ Missions/DiceMission.py
@@ -25,27 +25,27 @@ Dice2 = Dice()
 def run_Dice():
     counter = 0
     while check_pair() == 0:
-        forward_sleep(30,1)
-        counter +=1
-        if counter==5:
+        forward_sleep(30, 1)
+        counter += 1
+        if counter == 5:
             print("mission failed we'll get em next time")
             break
     while check_bigdice(Dice1):#break while when dice is biggerthan boundingbox
         if check_binding_box(Dice1):
-            forward_sleep(40,2)
+            forward_sleep(40, 2)
         else:
             align()
         update_dice(Dice1)
-    forward_sleep(40,3)
-    backward_sleep(40,6) #retrace steps
+    forward_sleep(40, 3)
+    backward_sleep(40, 6) #retrace steps
     update_dice(Dice2)
     while check_bigdice(Dice2):#break while when dice is biggerthan boundingbox
         if check_binding_box(Dice2):
-            forward_sleep(40,2)
+            forward_sleep(40, 2)
         else:
             align()
         update_dice(Dice2)
-    forward_sleep(40,3)
+    forward_sleep(40, 3)
     #sets finised state, end
 
 
@@ -55,65 +55,65 @@ def align(Dice):  # movement values need to be adjusted
     current_depth = RosCom.getDepth()
     update_dice(Dice)
     if Dice.get_centx() < camCenterX and Dice.get_centy() < camCenterY:
-        left_sleep(40,1)
+        left_sleep(40, 1)
         RosCom.setDepth(current_depth - 1)
     elif Dice.get_centx() < camCenterX and Dice.get_centy() > camCenterY:
-        left_sleep(40,1)
+        left_sleep(40, 1)
         RosCom.setDepth(current_depth + 1)
     elif Dice.get_centx() > camCenterX and Dice.get_centy() > camCenterY:
-        right_sleep(40,1)
+        right_sleep(40, 1)
         RosCom.setDepth(current_depth + 1)
     elif Dice.get_centx() > camCenterX and Dice.get_centy() < camCenterY:
-        right_sleep(40,1)
+        right_sleep(40, 1)
         RosCom.setDepth(current_depth - 1)
     elif Dice.get_centx() == camCenterX and Dice.get_centy() < camCenterY:
         RosCom.setDepth(current_depth - 1)
     elif Dice.get_centx() == camCenterX and Dice.get_centy() > camCenterY:
         RosCom.setDepth(current_depth + 1)
     elif Dice.get_centx() < camCenterX and Dice.get_centy() == camCenterY:
-        left_sleep(40,1)
+        left_sleep(40, 1)
     elif Dice.get_centx() > camCenterX and Dice.get_centy() == camCenterY:
-        right_sleep(40,1)
+        right_sleep(40, 1)
 
 """"Here we are gonna check all patterns possible for 7 and 11, if such pattern exists then we go with that one"""
 def check_pair():
     if visionlist[4] is not None and visionlist[5] is not None:
-        Dice1.set_centx(visionlist[4].getcentx()) #upadte for vision
-        Dice1.set_centy(visionlist[4].getcenty())  # upadte for vision
+        Dice1.set_centx(visionlist[4].getcentx()) #update for vision
+        Dice1.set_centy(visionlist[4].getcenty())  # update for vision
         Dice1.set_value(5)
         Dice1.set_boundingbox(visionlist[4].getbounding) #needs update
-        Dice2.set_centx(visionlist[5].getcentx())  # upadte for vision
-        Dice2.set_centy(visionlist[5].getcenty())  # upadte for vision
+        Dice2.set_centx(visionlist[5].getcentx())  # update for vision
+        Dice2.set_centy(visionlist[5].getcenty())  # update for vision
         Dice2.set_value(6)
         Dice2.set_boundingbox(visionlist[5].getbounding)  # needs update
         return 11
     elif visionlist[5] is not None and visionlist[0]is not None:
-        Dice1.set_centx(visionlist[0].getcentx())  # upadte for vision
-        Dice1.set_centy(visionlist[0].getcenty())  # upadte for vision
+        Dice1.set_centx(visionlist[0].getcentx())  # update for vision
+        Dice1.set_centy(visionlist[0].getcenty())  # update for vision
         Dice1.set_value(1)
         Dice1.set_boundingbox(visionlist[0].getbounding)  # needs update
-        Dice2.set_centx(visionlist[5].getcentx())  # upadte for vision
-        Dice2.set_centy(visionlist[5].getcenty())  # upadte for vision
+        Dice2.set_centx(visionlist[5].getcentx())  # update for vision
+        Dice2.set_centy(visionlist[5].getcenty())  # update for vision
         Dice2.set_value(6)
         Dice2.set_boundingbox(visionlist[5].getbounding)  # needs update
         return 7
     elif visionlist[4] is not  None and visionlist[1] is not None:
-        Dice1.set_centx(visionlist[4].getcentx())  # upadte for vision
-        Dice1.set_centy(visionlist[4].getcenty())  # upadte for vision
+        Dice1.set_centx(visionlist[4].getcentx())  # update for vision
+        Dice1.set_centy(visionlist[4].getcenty())  # update for vision
         Dice1.set_value(5)
         Dice1.set_boundingbox(visionlist[4].getbounding)  # needs update
-        Dice2.set_centx(visionlist[1].getcentx())  # upadte for vision
-        Dice2.set_centy(visionlist[1].getcenty())  # upadte for vision
+        Dice2.set_centx(visionlist[1].getcentx())  # update for vision
+        Dice2.set_centy(visionlist[1].getcenty())  # update for vision
         Dice2.set_value(2)
         Dice2.set_boundingbox(visionlist[1].getbounding)  # needs update
         return 7
     elif visionlist[2] is not None and visionlist[3] is not None:
-        Dice1.set_centx(visionlist[2].getcentx())  # upadte for vision
-        Dice1.set_centy(visionlist[2].getcenty())  # upadte for vision
+        Dice1.set_centx(visionlist[2].getcentx())  # update for vision
+        Dice1.set_centy(visionlist[2].getcenty())  # update for vision
         Dice1.set_value(3)
         Dice1.set_boundingbox(visionlist[2].getbounding)  # needs update
-        Dice2.set_centx(visionlist[3].getcentx())  # upadte for vision
-        Dice2.set_centy(visionlist[3].getcenty())  # upadte for vision
+        Dice2.set_centx(visionlist[3].getcentx())  # update for vision
+        Dice2.set_centy(visionlist[3].getcenty())  # update for vision
         Dice2.set_value(4)
         Dice2.set_boundingbox(visionlist[3].getbounding)  # needs update
         return 7
@@ -121,9 +121,17 @@ def check_pair():
         return 0
 
 
-def check_bigdice(Dice):  # checks 4 out of bound dice
-
-    return True
+def check_bigdice(Dice):  # checks 4 out of bound dice (Note: input variables)
+    if x_dice_up_right > x_bounding_up_right and y_dice_up_right < y_bounding_up_right and x_dice_down_right < x_bounding_down_right and y_dice_down_right > y_bounding_down_right:
+        return False
+    elif x_dice_up_left < x_bounding_up_left and y_dice_up_left < y_bounding_up_left and x_dice_down_left < x_bounding_down_left and y_dice_down_left > y_bounding_down_left:
+        return False
+    elif x_dice_up_right > x_bounding_up_right and y_dice_up_right < y_bounding_up_right and x_dice_up_left < x_bounding_up_left and y_dice_up_left > y_bounding_up_left:
+        return False
+    elif x_dice_down_right > x_bounding_down_right and y_dice_down_right > y_bounding_down_right and x_dice_down_left < x_bounding_down_left and y_dice_down_left > y_bounding_down_left:
+        return False
+    else:
+        return True
 
 def forward_sleep(intensity, timer):
     RosCom.moveForward(intensity)
